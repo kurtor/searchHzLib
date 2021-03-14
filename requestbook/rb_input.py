b6454039993e0ac98e0f2aea43a7105d78abfb65
kurtor <zjuygp@gmail.com>
2021-03-14 16:44:48 +0800

    V2.0

--- requestbook/rb_input.py
+++ requestbook/rb_input.py
@@ -2,17 +2,60 @@ import time
 
 def getListFromTxt(path):
     book_name_list = []
-    with open(path,'r', encoding='UTF-8') as f:
+    with open(path,'r', encoding='utf-8') as f:
         data = f.readlines()
-
         for line in data:
-            if len(line.strip()) != 0 :
-                name = line.split('《')[1].split("》")[0]
-                book_name_list.append(name)
-
+            if (len(line.strip()) != 0 ):
+                name_author = line.strip().split('《')[1].split("》")
+                if(len(name_author) == 2):
+                    book_name_list.append([name_author[0].strip(),name_author[1].strip()])
+                elif(len(name_author) == 1):
+                    book_name_list.append([name_author[0].strip(),""])
     return book_name_list
 
 def getTimestamp():
     now = int(round(time.time()*1000))
     timestamp = time.strftime('%Y%m%d_%H%M%S',time.localtime(now/1000))
     return timestamp
+
+def getRealurl(book_title,book_author):
+    #按书名单独检索
+    web_domain = 'http://my1.hzlib.net'
+    web_location = '/opac/search?'
+    searchWay = 'searchWay=title200a'
+    q = '&q='
+    searchSource = '&searchSource=reader&scWay=dim&marcformat='
+    sortWay = '&sortWay=score'
+    sortOrder = '&sortOrder=desc'
+    publicTime = '&startPubdate=&endPubdate=&rows=10'
+
+    #按书名+作者检索
+    mul_web_domain = 'http://my1.hzlib.net'
+    mul_web_location = '/opac/search?'
+    mul_searchWay0 = 'searchWay=title'
+    mul_q0 = '&q0='
+    mul_logical0 = '&logical0=AND'
+    mul_searchWay1 = '&searchWay1=author'
+    mul_q1 = '&q1='
+    mul_logical1 = '&logical1=AND'
+    mul_searchWay2 = '&searchWay2='
+    mul_q2 = '&q2='
+    mul_searchSource = '&searchSource=reader&marcformat='
+    mul_sortWay = '&sortWay=score'
+    mul_sortOrder = '&sortOrder=desc'
+    mul_publicTime = '&startPubdate=&endPubdate=&rows=10'
+
+    real_url= ''
+        
+    if(book_author != ''):
+        #按书名+作者检索
+        real_url =  mul_web_domain + mul_web_location + \
+                    mul_searchWay0 + mul_q0 + book_title  + mul_logical0 + \
+                    mul_searchWay1 + mul_q1 + book_author + mul_logical1 + \
+                    mul_searchWay2 + mul_q2 + mul_searchSource + mul_sortWay + mul_sortOrder + mul_publicTime
+    else:
+        #按书名单独检索
+        real_url = web_domain + web_location + searchWay + q + book_title + searchSource + \
+            sortWay + sortOrder + publicTime
+
+    return real_url
--- requestbook/rb_spyder.py
+++ requestbook/rb_spyder.py
@@ -1,47 +1,43 @@
-from rb_input import getListFromTxt
-from rb_input import getTimestamp
+from rb_input import getListFromTxt, getTimestamp, getRealurl
 from rb_get import getSearchResult
-
 from requests_html import HTMLSession
+from pypinyin import lazy_pinyin
 import pandas as pd
 import os
-
-
+import json
 
 if __name__ == "__main__":
     
     useProxy = True #默认开启代理，不开启则置为False
-    endPage = 4 #最多搜索到第4页为止，即搜索前3页
+    endPage = 3 #最多搜索到第4页为止，即搜索前2页
+    web_domain = 'http://my1.hzlib.net'
+    input_path = './douban.txt'
+    output_path = './bookList_' + getTimestamp() + '.json'
     
     session = HTMLSession()
+    title_author_list = getListFromTxt(input_path)
+    all_obj_array = []
+    
+    for item in title_author_list:
+        title = item[0]
+        author = item[1]
+        print("downloading " + title + "  "  + author)
+        obj_array = []
+        which_book_count = 0 
+        max_try = 25
+        url = getRealurl(title, author)   
+         
+        obj_array = getSearchResult(obj_array, url, title, author, which_book_count, session, web_domain, useProxy, endPage, max_try)
+        book = {
+            "title": item[0],
+            "author": item[1],
+            "main_img": "mainImg/"+'_'.join(lazy_pinyin(title)) + ".jpg",
+            "content":"简介",
+            "classification": "分类",
+            "borrow_info": obj_array
+        }
+        all_obj_array.append(book)
 
-    # title_word_chinese_list = getListFromTxt('./douban.txt')
-    output_path = './bookList_' + getTimestamp() + '.xlsx'
-    if not os.path.exists(output_path):
-        new_pf = pd.DataFrame()
-        new_pf.to_excel(output_path,sheet_name='Data1')
-
-    title_word_chinese_list = ['地心游记']
-
-    web_domain = 'http://my1.hzlib.net'
-    web_location = '/opac/search?'
-    searchWay = 'searchWay=title200a'
-    searchSource = '&searchSource=reader&scWay=dim&marcformat='
-    sortWay = '&sortWay=score'
-    sortOrder = '&sortOrder=desc'
-    publicTime = '&startPubdate=&endPubdate=&rows=10'
-    libCode_hangzhou = '&logical0=AND&curlibcode=0000'
-
-    for title_word_chinese in title_word_chinese_list:
-        q_title = '&q=' + title_word_chinese
-        real_url = web_domain + web_location + searchWay + q_title + searchSource + \
-            sortWay + sortOrder + publicTime + libCode_hangzhou
-
-        temp_data = pd.DataFrame()
-        data_i = getSearchResult(temp_data, real_url, session, web_domain, useProxy, endPage)
-        if(data_i.empty):
-            print("no result for" + title_word_chinese)
-        else:
-            df_old = pd.DataFrame(pd.read_excel(output_path,sheet_name='Data1'))
-            df_rows = df_old.shape[0] #获取原数据的行数
-            data_i.to_excel(output_path, sheet_name='Data1',startcol=df_rows+1, index=False,header=False)
+        json_file = open(output_path,'w',encoding='utf-8')
+        print(json.dumps(all_obj_array,ensure_ascii=False),file = json_file)
+        json_file.close()
